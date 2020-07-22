76fe1b2693029cf39e7235f0cb3838692bce4117
zakharov-94 <thetytn@gmail.com>
2020-07-22 22:37:17 +0300

    add global exceptions handling

---
+++ MyLifeStory.API/Middleware/ExceptionHandleMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyLifeStory.API.Middleware
+{
+    public class ExceptionHandleMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandleMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (ValidationException ex)
+            {
+                await HandleException(context, HttpStatusCode.BadRequest, ex);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                await HandleException(context, HttpStatusCode.Unauthorized, ex);
+            }
+            catch (ApplicationException ex)
+            {
+                await HandleException(context, HttpStatusCode.BadRequest, ex);
+            }
+            catch(Exception ex)
+            {
+                //Log information
+                await HandleException(context, HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        private async Task HandleException(HttpContext context, HttpStatusCode statusCode, Exception exception)
+        {
+            context.Response.Clear();
+            context.Response.StatusCode = (int)statusCode;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(exception.Message);
+        }
+    }
+}
--- MyLifeStory.API/Startup.cs
+++ MyLifeStory.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyLifeStory.API.Middleware;
 using MyLifeStory.BLL;
 using MyLifeStory.DAL;
 
@@ -32,6 +33,8 @@ namespace MyLifeStory.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMiddleware<ExceptionHandleMiddleware>();
 
             app.UseHttpsRedirection();
 
